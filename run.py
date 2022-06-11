bd33f71bc1fefeedcb7eb0ce9408859a48ec5938
Henry Leon <henryatusabes@gmail.com>
2022-06-11 12:15:13 +0000

    Initial commit I set up my workspace with all necessary files

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# External import statements 
+import random
+
+# Import statements I created for the game
+from words import english_words
+from words import spanish_words
+from words import french_words
+from display import display_hangman
+
+
+
+
---
+++ words.py
@@ -0,0 +1,32 @@
+# List of English words for the game 
+
+english_words = [
+    "PHYTON",
+    "ROYAL",
+    "JUBILEE",
+    "PARTY",
+    "ABOUT",
+    "MAGIC"
+]
+
+# List of Spanish words for the game
+
+spanish_words = [
+    "HOLA",
+    "SOLEADO",
+    "PLAYA",
+    "COMER",
+    "FIESTA",
+    "AHORA",
+]
+
+# List of French words for the game
+
+french_words = [
+    "MERCI",
+    "FEMME",
+    "TEMPS",
+    "MONDE",
+    "MONSIEUR",
+    "RAISON",
+]
