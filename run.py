66598bc70a6822f568ab71966e1c876cc21434f8
Henry Leon <henryatusabes@gmail.com>
2022-06-11 12:41:02 +0000

    Create functions to welcome player to the game

--- run.py
+++ run.py
@@ -10,6 +10,41 @@ from words import spanish_words
 from words import french_words
 from display import display_hangman
 
+# Welcome message to Hangman game
+def type_name():
+    """
+    Welcome message and input player name to the console
+    """
+    print("###################################################################")
+    print("                       HANGMAN")
+    print("Choose your preferred language and guess the word to win the game!")
+    print("###################################################################")
+
+    # Print players name with welcome message
+    while True:
+        player_name = input("\033[0;36mEnter your name here: ").strip()
+
+        if validate_name(player_name):
+            print(f"Hello {player_name} welcome to my game\n")
+            break
+
+    # Game wont run without a name
+    def validate_data(name):
+    """
+    Validates user data for name
+    """
+
+    try:
+        if name == "":
+            raise ValueError("\033[0;31mPlease input a name \n")
+        elif len(name.strip()) == 0:
+            raise ValueError("\033[0;31mPlease input a name \n")
+
+    except ValueError as error:
+        print(f"\033[0;31mPlease try again. {error}")
+        return False
+
+    return True
 
 
 
